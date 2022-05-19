202c6f990e59de9ca374a654de03d5324036d8ab
99ehddud <99ehddud@gmail.com>
2022-05-19 15:30:23 +0900

    Practice Ver.1.0.0 (Make Json File)

---
+++ Practice.java
@@ -0,0 +1,9 @@
+package test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Practice {
+    public static void main(String[] args) {
+        ObjectMapper mapper = new ObjectMapper();
+    }
+}
